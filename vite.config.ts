bf69b634acf1b6705a8e28fdd323ec5a4918ce68
mulleresposito <mulleresposito@hotmail.com>
2025-03-05 23:38:59 -0400

    refactor(tsconfig): consolidate TypeScript configuration and add path aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { fileURLToPath } from 'url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@assets": fileURLToPath(new URL("src/assets", import.meta.url)),
+      "@icons": fileURLToPath(new URL("src/assets/icons", import.meta.url)),
+      "@components": fileURLToPath(new URL("src/components", import.meta.url)),
+    }
+  }
 })
